f242c14ed4ffb23e2505cec3afcccd0c12381f9b
vijayakumar.ayyavoo@toyota.com <vijayakumar.ayyavoo@toyota.com>
2023-04-24 23:41:07 +0530

    [major]: added few more initial features

--- release.config.js
+++ release.config.js
@@ -71,9 +71,9 @@ module.exports = {
 
             console.log("commit.hash:" + commit.hash)
   
-            // if (typeof commit.hash === `string`) {
-            //     commit.hash = commit.hash.substring(0, 7)
-            // }
+            if (typeof commit.hash === `string`) {
+                commit.shortHash = commit.hash.substring(0, 7)
+            }
   
             if (typeof commit.subject === `string`) {
                 let url = context.repository
