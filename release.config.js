7b08f9ed6f478bc54288430f5b4383b6865c9e63
vijayakumar.ayyavoo@toyota.com <vijayakumar.ayyavoo@toyota.com>
2023-04-25 13:15:56 +0530

    adhoc removed commented code

--- release.config.js
+++ release.config.js
@@ -17,19 +17,7 @@ module.exports = {
           releaseRules: [
             {message: "*[[]major[]]*", release: "major"},
             {message: "*[[]bug-fix[]]*", release: "patch"},
-            {message: "!(*[[]major[]]*|*[[]bug-fix[]]*)", release: "minor"},
-            // {breaking: true, release: 'major'},
-            // {type: 'build', release: 'patch'},
-            // {type: 'chore', release: 'patch'},
-            // {type: 'ci', release: 'patch'},
-            // {type: 'docs', release: 'patch'},
-            // {type: 'feat', release: 'minor'},
-            // {type: 'fix', release: 'patch'},
-            // {type: 'perf', release: 'patch'},
-            // {type: 'refactor', release: 'patch'},
-            // {type: 'revert', release: 'patch'},
-            // {type: 'style', release: 'patch'},
-            // {type: 'test', release: 'patch'}
+            {message: "!(*[[]major[]]*|*[[]bug-fix[]]*)", release: "minor"}
           ]
         }],
         '@semantic-release/release-notes-generator',
@@ -46,13 +34,7 @@ module.exports = {
       preset: 'conventionalcommits',
       writerOpts: {
         transform: (commit, context) => {
-            const issues = []
-  
-            // commit.notes.forEach(note => {
-            //     note.title = `BREAKING CHANGES`
-            // })
-            
-            // NOTE: Any changes here must be reflected in `CONTRIBUTING.md`.
+
             if (commit.message.startsWith("Merge branch")) {
                 return
             } else if (commit.message.includes("[major]")) {
@@ -61,28 +43,6 @@ module.exports = {
                 commit.type = `🐛 Bug-Fix`
             } else if (!(commit.message.includes("[major]") || commit.message.includes("[bug-fix]"))) {
                 commit.type = `📝 Minor Changes`
-            // } else if (commit.type === `feat`) {
-            //     commit.type = `Features`
-            // } else if (commit.type === `fix`) {
-            //     commit.type = `Bug Fixes`
-            // } else if (commit.type === `perf`) {
-            //     commit.type = `Performance Improvements`
-            // } else if (commit.type === `revert`) {
-            //     commit.type = `Reverts`
-            // } else if (commit.type === `docs`) {
-            //     commit.type = `Documentation`
-            // } else if (commit.type === `style`) {
-            //     commit.type = `Styles`
-            // } else if (commit.type === `refactor`) {
-            //     commit.type = `Code Refactoring`
-            // } else if (commit.type === `test`) {
-            //     commit.type = `Tests`
-            // } else if (commit.type === `build`) {
-            //     commit.type = `Build System`
-            // // } else if (commit.type === `chore`) {
-            // //     commit.type = `Maintenance`
-            // } else if (commit.type === `ci`) {
-            //     commit.type = `Continuous Integration`
             } else {
                 return
             }
@@ -94,7 +54,8 @@ module.exports = {
             if (typeof commit.hash === `string`) {
                 commit.shortHash = commit.hash.substring(0, 7)
             }
-  
+            
+            const issues = []
             if (typeof commit.subject === `string`) {
                 let url = context.repository
                     ? `${context.host}/${context.owner}/${context.repository}`
